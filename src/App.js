b56dc1c2e3b3109b229bd392f399eb8d23ccb3dc
manjunj <manjunj@student.unimelb.edu.au>
2021-12-27 03:56:19 +1100

    Add Homepage and some shop features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Homepage />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/shop" element={<ShopPage />}/>
+      </Routes>
     </BrowserRouter>
   )
 }
