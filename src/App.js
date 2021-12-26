04fc2091539f423d099e62e7d509be433dca8e58
manjunj <manjunj@student.unimelb.edu.au>
2021-12-27 01:48:12 +1100

    Set routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+
 
 function App() {
   return (
-    <Homepage />
-  );
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Homepage />}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./menu-item.styles.scss";
+import {useNavigate, useLocation} from 'react-router-dom';
+
+// function MenuItem(title, imageUrl, size, linkUrl, history, match){
+function MenuItem({title, imageUrl, size, linkUrl}){
+    const location = useLocation();
+    const navigate = useNavigate();
+    return (
+    <div 
+    className = {"menu-item "+ size} 
+    onClick={() => navigate(location.pathname+linkUrl)}
+    >
+        <div className='background-image' style={{ backgroundImage :"url("+ imageUrl+ ")"}} />
+        <div className = 'content'>
+            <h1 className = 'title'>{title.toUpperCase()}</h1>  
+            <span className = 'subtitle'>SHOP NOW</span>  
+        </div>
+    </div>)
+}
+export default MenuItem;
